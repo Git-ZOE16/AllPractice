33f1170469f32a8013a4165848c09e2dd529a199
Zoe <zoeinspired1609@gmail.com>
2026-04-30 21:38:49 +0100

    Created a Game App to Check Even and Odd Numbers

---
+++ GameApp.java
@@ -0,0 +1,16 @@
+public class GameApp{
+public static void main(String[] args){
+
+int number = 20;
+
+if(number % 2 == 0){
+System.out.println("Even number");
+}
+
+else{
+
+System.out.println("Odd Number");
+ }
+
+ }
+}
