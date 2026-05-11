0c2225b9e932c6b425225da16cd6d631cd4a0086
Zoe <zoeinspired1609@gmail.com>
2026-05-12 00:54:51 +0100

    Wrote a method to get the First Element in a Array

---
+++ Methods Practice/PracticeOne.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+public class PracticeOne{
+public static void main(String[] args ){
+
+int[] number = {3,7,1,9,4};
+
+//System.out.println(Arrays.toString(number));
+
+System.out.println(number[2]);
+
+}
+
+
+}
---
+++ Methods Practice/PracticeTwo.java
@@ -0,0 +1,15 @@
+public class PracticeTwo{
+public static void main(String[] args){
+
+int[] numbers = {8,2,5,10};
+
+System.out.println(getFirstElement(numbers));
+
+}
+
+public static int getFirstElement(int[] numbers){
+
+return numbers[0];
+
+}
+}
