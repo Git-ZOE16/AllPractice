a9fefb1169b38b92ecc80c963f4dfa057db230d5
Zoe <zoeinspired1609@gmail.com>
2026-05-12 01:14:56 +0100

    Wrote a method to get the Last Element in an Array

---
+++ Methods Practice/PracticeThree.java
@@ -0,0 +1,22 @@
+public class PracticeThree{
+
+public static void main(String[] args){
+
+int[] numbers = {12,6,4,9};
+
+System.out.println(getLastElement(numbers));
+
+}
+
+public static int getLastElement(int[] numbers){
+
+return numbers[3];
+
+
+}
+
+
+
+
+
+}
