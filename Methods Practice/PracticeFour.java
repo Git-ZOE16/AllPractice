e16bd1ff9265ea040aea9e9764b27c5b229d4e68
Zoe <zoeinspired1609@gmail.com>
2026-05-12 02:13:25 +0100

    Method to find the Length of an Array

---
+++ Methods Practice/PracticeFour.java
@@ -0,0 +1,12 @@
+public class PracticeFour{
+public static void main(String[] args){
+
+int[] numbers = {1,2,3,4,5,8};
+System.out.println(getArrayLength(numbers));
+
+}
+public static int getArrayLength(int[] numbers){
+   
+return numbers.length;
+}
+} 
